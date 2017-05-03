5b8937ec692903c100acdf196c711680a6c445ae
蘇子耀 <simplesuhao@163.com>
2017-05-03 23:02:13 +0800

    2017-05-03

--- src/main/java/org/smart4j/chapter2/controller/CustomerCreateServlet.java
+++ src/main/java/org/smart4j/chapter2/controller/CustomerCreateServlet.java
@@ -1,7 +1,33 @@
 package org.smart4j.chapter2.controller;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
- * Created by Administrator on 2017/5/1.
+ * Created by suziyao on 2017/5/1.
+ * 客户创建
  */
-public class CustomerCreateServlet {
+@WebServlet("/customer_create")
+public class CustomerCreateServlet extends HttpServlet{
+    /**
+     * 进入客户创建界面
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException{
+        //todo
+    }
+
+    /**
+     * 处理创建客户请求
+     */
+    @Override
+    protected void  doPost(HttpServletRequest req, HttpServletResponse resq)
+        throws ServletException, IOException{
+        //todo
+    }
 }
